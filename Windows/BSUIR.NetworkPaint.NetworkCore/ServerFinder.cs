0cdfb0b1d16b16c68dfad991502dbf11337bb1c9
Ilya.Starostin <bkmz2005@yandex.ru>
2014-04-26 19:05:07 +0300

    Classical sockets was compleated

--- Windows/BSUIR.NetworkPaint.NetworkCore/ServerFinder.cs
+++ Windows/BSUIR.NetworkPaint.NetworkCore/ServerFinder.cs
@@ -13,93 +13,85 @@ using System.Xml.Serialization;
 
 namespace BSUIR.NetworkPaint.NetworkCore
 {
-	public class ServerFinder
-	{
-		private const int MaxWaitTime = 60;
+    public class ServerFinder
+    {
+        private const int MaxWaitTime = 60;
         private Socket _socket;
-		private IPEndPoint _endpoint;
-		private IPEndPoint _sendEndpoint;
-		private volatile bool _isDataAccepted;
-		private volatile ServerData _data;
+        private IPEndPoint _endpoint;
+        private IPEndPoint _sendEndpoint;
+        private volatile bool _isDataAccepted;
+        private volatile ServerData _data;
+        Thread reciverThread;
 
-		public ServerFinder(int port)
-		{
-			_endpoint = new IPEndPoint(IPAddress.Any, port);
-			_sendEndpoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), port+1);
+        public ServerFinder(int port)
+        {
+            _endpoint = new IPEndPoint(IPAddress.Any, port);
+            _sendEndpoint = new IPEndPoint(IPAddress.Parse("255.255.255.255"), port + 1);
             _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-
-            _socket.SetSocketOption(SocketOptionLevel.Udp, SocketOptionName.NoDelay, 1);
-     _socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, 1); 
+            _socket.EnableBroadcast = true;
             _socket.Bind(_endpoint);
 
-            //_socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(IPAddress.Parse("255.255.255.255"), IPAddress.Any));
-    // May want to set this:
-    _socket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 0); // only LAN
+            reciverThread = new Thread(() => ReciveData());
+        }
 
-		}
+        public ServerData Find()
+        {
+            var startTime = DateTime.Now;
 
-		public ServerData Find()
-		{
-			var startTime = DateTime.Now;
+            reciverThread.Start();
 
-            Thread q = new Thread(() => ReciveData());
-            q.Start();
-           // ReciveData();
-
-			while( (!_isDataAccepted) && ( (DateTime.Now - startTime).Seconds < MaxWaitTime ) )
-			{
-                SocketAsyncEventArgs e = new SocketAsyncEventArgs();
-                e.SetBuffer(new byte[0], 0, 0);
-
-                _socket.Connect(_sendEndpoint);
-                _socket.Send(new byte[0]);
+            while ((!_isDataAccepted) && ((DateTime.Now - startTime).Seconds < MaxWaitTime))
+            {
+                _socket.SendTo(new byte[0], _sendEndpoint);
                 Thread.Sleep(500);
-			}
+            }
 
-			if (_isDataAccepted)
-			{
-				return _data;
-			} else 
-			{
-				return null;
-			}
-		}
+            if (_isDataAccepted)
+            {
+                return _data;
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         public void ReciveData()
         {
             byte[] buffer = new byte[8192];
 
-            EndPoint converted = _endpoint;
+            int dataLength = 0;
 
-            _socket.ReceiveFrom(buffer, ref converted);
-       
+            while (!_isDataAccepted)
+            {
+                dataLength = _socket.Receive(buffer); 
+                if (dataLength > 0)
+                {
+                    ReciveCallback(buffer);
+                    buffer = new byte[8192];
+                }
+            }
         }
 
 
-		public void ReciveCallback(object sender, SocketAsyncEventArgs e)
-		{
-            
-			byte[] data = new byte[8192];
-
-            data = e.Buffer;
-
-			if (data.Length > 0)
-			{
-				MemoryStream stream = new MemoryStream(data);
-                //XmlSerializer formatter = new XmlSerializer(typeof(ServerData));
+        public void ReciveCallback(byte[] data)
+        {
+            if (data.Length > 0)
+            {
+                MemoryStream stream = new MemoryStream(data);
 
                 BinaryFormatter formatter = new BinaryFormatter();
 
-				stream.Seek(0, SeekOrigin.Begin);
+                stream.Seek(0, SeekOrigin.Begin);
 
                 var recivedData = (string)formatter.Deserialize(stream);
 
                 _data = CustomXmlSerializer.Deserialize<ServerData>(recivedData);
 
-				_isDataAccepted = true;
-			}
+                _isDataAccepted = true;
+            }
 
-			
-		}
-	}
+
+        }
+    }
 }
