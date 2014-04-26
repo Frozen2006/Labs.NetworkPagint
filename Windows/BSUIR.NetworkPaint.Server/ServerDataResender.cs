17dc5a687e53b8f807ded757827267c6b97c200f
Ilya.Starostin <bkmz2005@yandex.ru>
2014-04-26 19:33:03 +0300

    Server TCP connector was updated

--- Windows/BSUIR.NetworkPaint.Server/ServerDataResender.cs
+++ Windows/BSUIR.NetworkPaint.Server/ServerDataResender.cs
@@ -8,6 +8,7 @@ using System.Net.Sockets;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
@@ -17,34 +18,43 @@ namespace BSUIR.NetworkPaint.Server
 	public class ServerDataResender
 	{
 		private TcpListener _listner;
-		private List<TcpClient> _clients = new List<TcpClient>();
+		private List<Socket> _clients = new List<Socket>();
+        private Socket _socket;
+        private EndPoint endPoint;
+
 
 		public ServerDataResender(int port)
 		{
-			_listner = new TcpListener(IPAddress.Any, port);
-            _listner.Start();
+			/*_listner = new TcpListener(IPAddress.Any, port);
+                        _listner.Start();*/
+            endPoint = new IPEndPoint(IPAddress.Any, port);
+
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Bind(endPoint);
+
 			StartListen();
 		}
 
-		private void StartListen()
+		private async void StartListen()
 		{
-			_listner.BeginAcceptTcpClient(new AsyncCallback(ResendData), _listner);
+            _socket.Listen(Int32.MaxValue);
+            while(true)
+            {
+                var client = _socket.Accept();
+                Thread thread = new Thread(() => ResendData(client));
+                thread.Start();
+            }
 		}
 
-		private void ResendData(IAsyncResult result)
+		private void ResendData(Socket client)
 		{
             Console.WriteLine("Client connected!");
 
-            var listner = (TcpListener)result.AsyncState;
-
-            var client = _listner.EndAcceptTcpClient(result);
-            StartListen();
-            
 			_clients.Add(client);
 
             BinaryFormatter formatter = new BinaryFormatter();
 
-			var stream = client.GetStream();
+            var stream = new NetworkStream(client);
 
             
             TransferPackage data = null;
@@ -79,12 +89,18 @@ namespace BSUIR.NetworkPaint.Server
                         return;
                     }
                 }
+                catch(IOException)
+                {
+                    Console.WriteLine("Network problem. Client was disconnected without 'disconnect()' call");
+                    _clients.Remove(client);
+                    return;
+                }
                 
 				
 				foreach (var currentClient in _clients)
 				{
                     var outData = CustomXmlSerializer.Serialize<TransferPackage>(data);
-                    formatter.Serialize(currentClient.GetStream(), outData);
+                    formatter.Serialize(new NetworkStream(currentClient), outData);
 				}
 			}
 		}
